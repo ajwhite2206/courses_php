c006f2dc7a023b4e1e227fb2aeb5faa1cfc74c9f
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-09-11 20:24:37 -0700

    Create index page

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+	include 'datalogin.php';
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link type="text/css" href="styles/defaultstyle.css" rel="stylesheet" media="screen" />
+	<link type="text/css" href="styles/courses.css" rel="stylesheet" media="screen" />
+	<title>Professional Development and Training || Lewis-Clark State College</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+	<div id="container">
+		<div id="banner">
+			<!--Start of the header, and LCSC logo image -->
+			<div class="auto-style2">
+				<a href="http://connect.lcsc.edu/pdt"> <img alt="LCSC Blue Flag Logo" height="63" src="images/LCSCLogo-small.jpg" width="194"> </a>
+
+				<span class="auto-style1">
+					<strong><br>Professional Development &amp; Training (PDT)</strong>
+				</span>
+			</div>
+
+			<!-- strip just below banner -->
+			<h1>Login page</h1>
+		</div>
+
+		<div id="wrapper">
+			<body>
+				<div class="container">
+
+					
+
+				</div> <!-- /container -->
+
+			<br>
+			</body>
+		</div>
+		<div id="footer">
+			<h1>Lewis-Clark State College  ||  Professional Development and Training  ||  For more information please contact: jcrea@lcsc.edu</h1>
+		</div>
+	</div>
+</body>
+
+</html>
