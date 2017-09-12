35397c193eac46e1a2c848d93f37fbf61db6b2b6
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-09-11 20:47:24 -0700

    Adding register.php

---
+++ register.php
@@ -0,0 +1,89 @@
+<?php
+
+session_start();
+
+if( isset($_SESSION['email']) ){
+	header("Location: index.php");
+}
+
+require 'database.php';
+
+$message = '';
+
+if(!empty($_POST['email']) && !empty($_POST['password'])):
+	
+	// Enter the new user in the database
+	$sql = "INSERT INTO tblStudents (email, password) VALUES (:email, :password)";
+	$stmt = $conn->prepare($sql);
+
+	$stmt->bindParam(':email', $_POST['email']);
+	$stmt->bindParam(':password', password_hash($_POST['password'], PASSWORD_BCRYPT));
+
+	if( $stmt->execute() ):
+		$message = 'Successfully created new user';
+	else:
+		$message = 'Sorry there must have been an issue creating your account';
+	endif;
+
+endif;
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link type="text/css" href="styles/defaultstyle.css" rel="stylesheet" media="screen" />
+	<link type="text/css" href="styles/courses.css" rel="stylesheet" media="screen" />
+	<title>Professional Development and Training || Lewis-Clark State College</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+	<div id="container">
+		<div id="banner">
+			<!--Start of the header, and LCSC logo image -->
+			<div class="auto-style2">
+				<a href="http://connect.lcsc.edu/pdt"> <img alt="LCSC Blue Flag Logo" height="63" src="images/LCSCLogo-small.jpg" width="194"> </a>
+
+				<span class="auto-style1">
+					<strong><br>Professional Development &amp; Training (PDT)</strong>
+				</span>
+			</div>
+
+			<!-- strip just below banner -->
+			<h1>register page</h1>
+		</div>
+
+		<div id="wrapper">
+			<body>
+				<div class="container">
+
+					<?php if(!empty($message)): ?>
+		<p><?= $message ?></p>
+	<?php endif; ?>
+
+	<h1>Register</h1>
+	<span>or <a href="login.php">login here</a></span>
+
+	<form action="register.php" method="POST">
+		
+		<input type="text" placeholder="Enter your email" name="email">
+		<input type="password" placeholder="and password" name="password">
+		<input type="password" placeholder="confirm password" name="confirm_password">
+		<input type="submit">
+
+	</form>
+
+			<br>
+			</body>
+		</div>
+		<div id="footer">
+			<h1>Lewis-Clark State College  ||  Professional Development and Training  ||  For more information please contact: jcrea@lcsc.edu</h1>
+		</div>
+	</div>
+</body>
+
+</html>
