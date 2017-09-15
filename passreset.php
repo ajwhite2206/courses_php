b5955d168aad14b11db70285b52af21b5215d46e
ajwhite2206 <ajwhite2206@lcmail.lcsc.edu>
2017-09-15 11:44:06 -0700

    yayyyyy

--- passreset.php
+++ passreset.php
@@ -7,31 +7,20 @@
 		//loop to make sure form was filled out correctly
 		$code = $_SESSION['code'];
 		$email = $_SESSION['email'];
+
 		if(!empty($_GET["newpass1"])){
 			if($_GET["resetcode"] == $code){
 				if(($_GET["newpass1"]) == ($_GET["newpass2"])){
-					//if all form credentials are met password is updated in access
-					
-					//this is not working
-					echo $_GET["newpass1"];
-					echo $email;
-					$qry = "UPDATE tblStudents SET password = " . $_GET['newpass1'] . "WHERE email='ajwhite2206@lcmail.lcsc.edu'";
-					$query = $conn->prepare(/*"UPDATE tblStudents SET password = ? WHERE email='ajwhite2206@lcmail.lcsc.edu'"*/$qry);
-					//$query->bindParam(1, $_GET["newpass1"], PDO::PARAM_STR);
-					//$query->execute();
-					while($r = $query->fetch(PDO::FETCH_OBJ)){
-						echo 'should be successful';
-					}
-					
-					
-				/* 	thisworksssss
-					$query = $conn->query("UPDATE tblStudents SET password = 'warrior' WHERE email='ajwhite2206@lcmail.lcsc.edu'");
-					while($r = $query->fetch(PDO::FETCH_OBJ)){
-					} */
-					
-					 
+                    //if all form credentials are met password is updated in access
+                    $password = $_GET["newpass1"];
 
-				// this is all the error handling if the form was not filled out correctly
+                    $query = $conn->query("UPDATE tblStudents SET password = '$password' WHERE email= '$email'");
+                    while($r = $query->fetch(PDO::FETCH_OBJ)){
+                        echo '<p>Successfully changed password.</p>';
+                    }
+					
+					
+					
 				} else {
 					echo 'Passwords do not match.';
 				}
@@ -48,7 +37,7 @@
 	<!-- beginning of HTML form -->
 	<form action="passreset.php" method="get">
 		<h2>Password Reset</h2>
-		<p>Email sent: <?php echo $_SESSION['savedemail'];?></p>
+		<p>Email sent: <?php echo $_SESSION['email'];?></p>
 		<input name="resetcode" id="resetcode" type="text" class="form-control" placeholder="Reset Code" autofocus></br>
 		<input name="newpass1" id="newpass1" type="text" class="form-control" placeholder="New Password" autofocus></br>
 		<input name="newpass2" id="newpass2" type="text" class="form-control" placeholder="Repeat New Password" autofocus></br>
