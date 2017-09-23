0d5ad09a828f45fdf95ce198e2e730c2d5b83d3b
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-09-23 13:46:48 -0700

    Modify front end

--- includes/header.php
+++ includes/header.php
@@ -39,13 +39,6 @@ echo '
           </ul>
         </li>
       </ul>
- 
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
       <ul class="nav navbar-nav navbar-right">';
       if( isset($_SESSION['email']) ){
         echo '  <li><a href="logout.php">sign out</a></li>';
