180179576578294fece0222515ff7299a25b77a8
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-09-23 10:59:51 -0700

    modify html index.php

--- index.php
+++ index.php
@@ -1,45 +1,26 @@
 <?php
 	include 'datalogin.php';
+  include("includes/header.html");
 
 ?>
 <!DOCTYPE html>
 <html>
-
-<head>
-	<!-- <link type="text/css" href="styles/defaultstyle.css" rel="stylesheet" media="screen" /> -->
-	<link type="text/css" href="https://bootswatch.com/flatly/bootstrap.min.css" rel="stylesheet" media="screen" />
-	
-	<title>Professional Development and Training || Lewis-Clark State College</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-
-<?php include("includes/header.html"); ?>
-
-<body>
-
-
-
-
-
-		<div id="wrapper">
 			<body>
 				<div class="container">
-
 					<div class="jumbotron">
-  <h1>lewis clark state college PDT</h1>
-  <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <p><a class="btn btn-primary btn-lg">Learn more</a></p>
-</div>
+                <h1>lewis clark state college PDT</h1>
+                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+          </div>
 
 				</div> <!-- /container -->
 
 			<br>
 			</body>
-		</div>
+	
 		<div id="footer">
 			
 		</div>
 	</div>
-</body>
 
 </html>
