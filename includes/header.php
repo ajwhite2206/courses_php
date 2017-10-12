822bf08dc664de4779e5b6e933b676fd921587ad
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-10-11 20:05:19 -0700

    Modify header

--- includes/header.php
+++ includes/header.php
@@ -29,9 +29,11 @@ echo '
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="courses.php">Courses<span class="sr-only">(current)</span></a></li>
-        <li><a href="register.php">Register<span class="sr-only">(current)</span></a></li>
+        <li><a href="courses.php">Courses<span class="sr-only">(current)</span></a></li>';
+        if(!(isset($_SESSION['email'])) ){
+        echo '<li><a href="register.php">Register<span class="sr-only">(current)</span></a></li>
 	<li><a href="recovery.php">Recover Account<span class="sr-only">(current)</span></a></li>';
+}
         if( isset($_SESSION['email']) ){
           echo '<li><a href="mycourses.php">My Courses<span class="sr-only">(current)</span></a></li>';
         }
