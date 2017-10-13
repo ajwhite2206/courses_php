82eaf8c5c3d619b800f4f5ce806991e8dda0646f
Alyissa <ajwhite2206@lcmail.lcsc.edu>
2017-10-12 20:01:01 -0700

    Survey hyper links on/off, and course eval finished

--- mycourses.php
+++ mycourses.php
@@ -25,24 +25,43 @@
 
         <?php
             $email = $_SESSION['email'];
-						$query = $conn->query("SELECT r.courseID, c.subject, c.instructor, c.location, c.startDate, c.endDate, r.registerDate, r.courseComplete FROM registration r, tblCourses c WHERE r.email='$email' AND r.courseID=c.CourseID;");
-						while($r = $query->fetch(PDO::FETCH_OBJ)) {
-              $stDate = date_create($r->startDate);
-              $edDate = date_create($r->endDate);
+			$query = $conn->query("SELECT r.courseID, c.subject, c.instructor, c.location, c.startDate, c.endDate, r.registerDate, r.courseComplete FROM registration r, tblCourses c WHERE r.email='$email' AND r.courseID=c.CourseID;");
+				
+				while($r = $query->fetch(PDO::FETCH_OBJ)) {
+					$stDate = date_create($r->startDate);
+					$edDate = date_create($r->endDate);
 
-              if($r->courseComplete == 0){ $courseCmp = 'Incomplete'; } elseif ($r->courseComplete == 1) { $courseCmp = 'Complete'; }
-
-					echo '<tbody>
+					if($r->courseComplete == 0){ $courseCmp = 'Incomplete'; } elseif ($r->courseComplete == 1) { $courseCmp = 'Complete'; }
+					
+					//checks to see if current courseid and email have completed course evaluation
+					$eval = $conn->query("SELECT CourseID FROM evaluation WHERE Email='$email'");
+						while($e = $eval->fetch(PDO::FETCH_OBJ)){
+							if ($e->CourseID == $r->courseID){
+								$match = True;
+								echo "finished";
+							} else {
+								$match = False;
+								echo "incomplete";
+							}
+						} 
+						
+						echo '<tbody>
 							<tr>
 								<td>'; echo $r->courseID, '</td>
 								<td>'; echo "<a href='courses_description.php?CID=$r->courseID'>$r->subject</a>", '</td>
 								<td>'; echo $r->instructor, '</td>
-                <td>'; echo $r->location, '</td>
-                <td>'; echo date_format($stDate, "m-d-Y"), '</td>
-                <td>'; echo date_format($edDate, "m-d-Y"), '</td>
+								<td>'; echo $r->location, '</td>
+								<td>'; echo date_format($stDate, "m-d-Y"), '</td>
+								<td>'; echo date_format($edDate, "m-d-Y"), '</td>
 								<td>'; echo $r->registerDate, '</td>
-                <td>'; echo $courseCmp, '</td>
-                <td>'; echo "<a href='survey.php?CID=$r->courseID'>Evaluation Form</a>", '</td>
+								<td>'; echo $courseCmp, '</td>
+								<td>';  
+									// prints hyper link if survey has not been completed
+									if($match){
+											echo "Evaluation Completed"; 
+									    } else { 
+											echo "<a href='survey.php?CID=$r->courseID'>Evaluation Form</a>";
+										} '</td>; 
 							</tr></tbody>';
 
 						}
--- survey.php
+++ survey.php
@@ -3,60 +3,60 @@
 	if (empty($_SESSION)){
 		header("Location: index.php");
 	}
-	
-	
-	$courseID = $_REQUEST['CID'];
 
 ?>
+
 <div class="container">
+
+	<!-- form for course evaluation -->
 	<form method="POST" action="surveycomplete.php" role="form" class="form-hoizontal">
 		<fieldset>
 
 
 			<legend>Survey</legend>
 			<div class="form-group">
+				<!-- This passes over CID for query-->
 				<input type="hidden" name="CID" id="CID" value="<?php echo $_REQUEST['CID'];?>" />
-				<input type="hidden" name="post-feedback" value="1">
 
-					<legend>When did you take the course?</legend>
-					<label for="date">
-						<input type="date" id="date" name="date" required="required">
-					</label>
-			   
-					<legend>Please rate this course with 1 being the lowest and 5 being the highest.</legend>
-					<label for="rating" required="required">
-						<select name="rating" id="rating">
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-						</select>
-					</label>
-			   
-					<legend>Would you recommend this course to a friend?</legend>
-					<label for="recommend-yes">
-						<input type="radio" name="recommend" id="recommend-yes" value="true">
-						Yes
-					</label>
+				<legend>When did you take the course?</legend>
+				<label for="date">
+					<input type="date" id="date" name="date" required="required">
+				</label>
+		   
+				<legend>Please rate this course with 1 being the lowest and 5 being the highest.</legend>
+				<label for="rating" required="required">
+					<select name="rating" id="rating">
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+					</select>
+				</label>
+		   
+				<legend>Would you recommend this course to a friend?</legend>
+				<label for="recommend-yes">
+					<input type="radio" name="recommend" id="recommend-yes" value="true">
+					Yes
+				</label>
 
-					<label for="recommend-no">
-						<input type="radio" name="recommend" id="recommend-no" value="false">
-						No
-					</label>
+				<label for="recommend-no">
+					<input type="radio" name="recommend" id="recommend-no" value="false">
+					No
+				</label>
+				
+				
+				<legend>Please leave comments below.</legend>
+				<textarea name="comment" id="comment"></textarea>
+				
+				<br>	
 					
-					
-					<legend>Please leave comments below.</legend>
-					<textarea name="comment" id="comment"></textarea>
-					
-					<br>	
-						
-					<div class="form-group">
-					  <div class="col-lg-10 col-lg-offset-2">
-						<button type="reset" class="btn btn-default">Cancel</button>
-						<button type="submit" class="btn btn-primary">Submit</button>
-					  </div>
-					</div>
+				<div class="form-group">
+				  <div class="col-lg-10 col-lg-offset-2">
+					<button type="reset" class="btn btn-default">Cancel</button>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				  </div>
+				</div>
 					
 			</div>
 		</fieldset>
