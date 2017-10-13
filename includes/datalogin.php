c3f1e189ef8f0df68b5896083958ecd8b31ff5a5
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-10-12 19:59:04 -0700

    commit

---
+++ includes/datalogin.php
@@ -0,0 +1,17 @@
+<?php
+$server = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = 'auth';
+
+$connStr =  "odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=C:\\xampp\\htdocs\\course.mdb;";
+//$connStr =  "odbc:DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=C:\\wamp\\www\\new-php\\db\\course.mdb;";
+
+
+try{
+	$conn = new PDO($connStr);
+} catch(PDOException $e){
+	die( "Connection failed: " . $e->getMessage());
+}
+
+?>
