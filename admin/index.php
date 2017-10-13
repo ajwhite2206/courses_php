a6395f5e4b83eba4022b95e889e4d2508bd13a17
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-10-13 10:48:18 -0700

    te

--- admin/index.php
+++ admin/index.php
@@ -28,6 +28,7 @@ if(!empty($_POST['date']) /*&& !empty($_POST['rating']) && !empty($_POST['recomm
     $description = $_POST['description'];
     $course_type_true = $_POST['course-type-true'];
     $course_type_false = $_POST['course-type-false'];
+    
 
 	// Enter the new user in the database
 	$query = $conn->query("INSERT INTO tblCourses (CourseID, points, subject, StartDate, startTime, endDate, endTime, instructor, resources, location, description) VALUES ('111', '$points', '$start_date', '$start_time', '$end_date', '$instructor', '$resources', '$location ', '$description')");
