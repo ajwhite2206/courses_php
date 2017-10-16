80eef2a65333985aa03ec0c0eb3615ade5bb1c28
Alyissa <ajwhite2206@lcmail.lcsc.edu>
2017-10-15 20:37:08 -0700

    admin priv.

--- includes/header.php
+++ includes/header.php
@@ -7,37 +7,41 @@ echo '
 <head>
 	<link type="text/css" href="https://bootswatch.com/flatly/bootstrap.min.css" rel="stylesheet" media="screen" />
 
-  <link type="text/css" href="https://bootswatch.com/flatly/bootstrap.css" rel="stylesheet" media="screen" />
+	<link type="text/css" href="https://bootswatch.com/flatly/bootstrap.css" rel="stylesheet" media="screen" />
 
-  <link type="text/css" https://bootswatch.com/flatly/variables.less" rel="stylesheet" media="screen" />
+	<link type="text/css" https://bootswatch.com/flatly/variables.less" rel="stylesheet" media="screen" />
 
-  <link rel="shortcut icon" type="image/png" href="images/logo.ico">
+	<link rel="shortcut icon" type="image/png" href="images/logo.ico">
 
 	<title>Professional Development and Training || Lewis-Clark State College</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 
 	<nav class="navbar navbar-default">
-  <div class="container-fluid">
+	<div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="index.php">PDT Website</a>
+		</button>
+		<a class="navbar-brand" href="index.php">PDT Website</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">';
-        if( isset($_SESSION['email']) ){
-					echo '<li><a href="courses.php">Courses<span class="sr-only">(current)</span></a></li>
-          <li><a href="mycourses.php">My Courses<span class="sr-only">(current)</span></a></li>';
-        } else {
-					echo '<li><a href="courses.php">Courses<span class="sr-only">(current)</span></a></li>
-          <li><a href="register.php">Register<span class="sr-only">(current)</span></a></li>
-					<li><a href="recovery.php">Recover Account<span class="sr-only">(current)</span></a></li>';
+        if( isset($_SESSION['email'])  && $_SESSION['admin'] == true){
+			echo '<li><a href="courses.php">Courses<span class="sr-only">(current)</span></a></li>
+			<li><a href="mycourses.php">My Courses<span class="sr-only">(current)</span></a></li>
+			<li><a href="mycourses.php">Admin Control<span class="sr-only">(current)</span></a></li>';
+        } elseif ( isset($_SESSION['email'])){
+			echo '<li><a href="courses.php">Courses<span class="sr-only">(current)</span></a></li>
+			<li><a href="mycourses.php">My Courses<span class="sr-only">(current)</span></a></li>';
+		} else {
+			echo '<li><a href="courses.php">Courses<span class="sr-only">(current)</span></a></li>
+			<li><a href="register.php">Register<span class="sr-only">(current)</span></a></li>
+			<li><a href="recovery.php">Recover Account<span class="sr-only">(current)</span></a></li>';
 				}
       echo '
       </ul>
